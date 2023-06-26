02dd7bb15d285f3ab8ee66731fadf08371bba6a4
root <root@DESKTOP-IE2I326.localdomain>
2023-06-26 16:17:16 +0300

    Code restructure

--- index.php
+++ index.php
@@ -1,60 +1,19 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Email Test</title>
+    <title>Send Email</title>
 </head>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
-<link rel="stylesheet" href="style.css">
 <body>
-<div class="bg-light">
-    <div class="container">
-        <div class="row justify justify-content-center">
-            <div class="col-11 col-md-8 col-lg-6 col-xl-5">
-                <form class="" method="POST" action="index.php">
-                    <div class="card bg-dark">
-                        <div class="row mt-0">
-                            <div class="col-md-12 ">
-                                <h4 class=""> Email Mailer💌</h4>
-                                </div>
-                        </div>
-                        <div class="form-group row mb-3">
-                            <div class="col-md-12 mb-0">
-                                <p class="mb-1">Fullname</p> <input id="e-mail" type="text" placeholder="Enter your full name" name="name" class="form-control input-box rm-border">
-                            </div>
+    <form class="" action="send.php" method="post">
+        Email <input type="email" name="email" value=""> <br>
+        Subject <input type="text" name="subject" value=""> <br>
+        Message <input type="text" name="message" value=""> <br>
+        <button type="submit" name="send">SEND</button>
 
-                            <div class="col-md-12 mb-0">
-                                <p class="mb-1">Email Address</p> <input id="e-mail" type="text" placeholder="Enter your email address" name="email" class="form-control input-box rm-border">
-                            </div>
 
-                            <div class="col-md-12 mb-0">
-                                <p class="mb-1">Subject</p> <input id="e-mail" type="text" placeholder="Enter subject here" name="subject" class="form-control input-box rm-border">
-                            </div>
-                            
-                            <div class="col-md-12 mb-0">
-                                <p class="mb-1">Phone</p> <input id="e-mail" type="text" placeholder="Enter your phone" name="phone" class="form-control input-box rm-border">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <div class="col-md-12 mb-2">
-                                <p class="mb-1">Message</p> <textarea id="message" type="text" placeholder="Enter your message" name="message" class="form-control input-box rm-border"></textarea>
-                            </div>
-                        </div>
-                        <div class="form-group row justify-content-center mb-0">
-                            <div class="col-md-12 px-3"> <input type="submit" value="Submit" name="submit" class="btn btn-block btn-purple rm-border"> </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>                                      
+    </form>
+    
 </body>
 </html>
-
-
